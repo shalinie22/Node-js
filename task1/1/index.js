4b32b83bbc8a2a8fee498dd0997ba461e2cb67f6
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-06-30 08:30:13 +0530

    Add files via upload

---
+++ task1/1/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 4000;
+const path = require("path");
+app.get("/", (req, res) => {
+  fs.writeFile("date-time.txt", SetTime("./date-time.txt"), (err) => {
+    if (err) throw err;
+    console.log("sets the date");
+  });
+  function SetTime(file) {
+    const { birthtime } = fs.statSync(file);
+    return birthtime;
+  }
+  fs.readFile("date-time.txt","utf8",(err,files)=>{
+    if (err) throw err;
+    res.send(files);
+  })
+});
+
+app.get("/list", (req, res) => {
+  fs.readdir(path.join(__dirname, "taskfolder"), (err, files) => {
+    if (err) throw err;
+    res.send(files);
+  });
+  console.log(__dirname);
+});
+app.listen(port, () => {
+  console.log(`server is listening ${port}`);
+});
